7323a520a90df6b4594e5f3b6f194bb1f6b45144
mannyjimen <manueljimenez0320>
2025-04-10 17:42:08 -0400

    defined functions specified by assignment

---
+++ SimOS.h
@@ -0,0 +1,42 @@
+#ifndef _SIM_OS_
+#define _SIM_OS_
+
+#include <string>
+#include <vector>
+#include <queue>
+
+struct FileReadRequest
+{
+    int PID{0};
+    std::string fileName{""};
+};
+
+struct MemoryItem
+{
+    unsigned long long itemAddress;
+    unsigned long long itemSize;
+    int PID; //PID of process using this chunk of memory
+};
+
+using MemoryUse = std::vector<MemoryItem>;
+
+constexpr int NO_PROCESS{-1};
+
+class SimOS{
+    SimOS(int numberOfDisks, unsigned long long amountOfRAM,
+    unsigned long long sizeOfOS);
+    bool NewProcess(unsigned long long size, int priority);
+    bool SimFork();
+    void SimExit();
+    void SimWait();
+    void DiskReadRequest(int diskNumber, std::string fileName);
+    void DiskJobCompleted(int diskNumber);
+    int GetCPU();
+    std::vector<int> GetReadyQueue();
+    MemoryUse GetMemory();
+    FileReadRequest GetDisk(int diskNumber);
+    std::queue<FileReadRequest> GetDiskQueue(int diskNumber);
+    
+};
+
+#endif //_SIM_OS_
