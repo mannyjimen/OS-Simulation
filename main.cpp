b38f63a8c6f19acc24e8500f0175ac48fd9f0c76
mannyjimen <manueljimenez0320@gmail.com>
2025-05-15 23:07:15 -0400

    added test function for disk management, printer helper

--- main.cpp
+++ main.cpp
@@ -18,23 +18,37 @@ void testForkAndWait(SimOS& mainOS){
     mainOS.printReadyQueue();
 }
 
+void testDiskManagement(SimOS& mainOS){
+    mainOS.NewProcess(100, 1); //PID 1
+    mainOS.NewProcess(200, 3); //PID 2
+    
+    std::cout << "All Disk Jobs before additions\n";
+    mainOS.printDisksAndJobs();
+    
+
+    mainOS.DiskReadRequest(2, "charlie");
+    mainOS.DiskReadRequest(1, "beta");
+    mainOS.DiskReadRequest(1, "wukong");
+    
+    std::cout << "All Disk Jobs after PID 2 insertions\n";
+    mainOS.printDisksAndJobs();
+    
+    mainOS.NewProcess(300, 5);
+    mainOS.SimExit();
+    mainOS.DiskReadRequest(2, "delta");
+    mainOS.DiskReadRequest(4, "epsilon");
+    std::cout << "All Disk Jobs after SimExit (erase pid2) and new requests\n";
+    mainOS.printDisksAndJobs();
+    
+}
+
 int main()
 {
     SimOS mainOS(5, 5'000'000, 1'000'000); // PID 1
-    mainOS.NewProcess(2'000, 3);
-    mainOS.NewProcess(2'000, 4);
-    mainOS.NewProcess(3'000, 1);
-    mainOS.NewProcess(4'000, 2);
-    mainOS.NewProcess(5'000, 6);
-    mainOS.SimExit();
-    mainOS.NewProcess(7'000, 9);
-    mainOS.NewProcess(1'000, 1);
-    mainOS.NewProcess(2'000, 2);
-    mainOS.SimExit();
-
+    
     // mainOS.printReadyQueue();
     // mainOS.printMemoryLayout();
     // mainOS.printMemoryHoles();
     // testForkAndWait(mainOS);
-    
+    testDiskManagement(mainOS);
 }
